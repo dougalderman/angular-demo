52b983e95e4bef776fffefece25776ac3c75d435
Doug Alderman <agnadcp-d1@yahoo.com>
2015-12-15 05:31:16 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+var foreman = angular.module('dm8', []);
+
+foreman.controller('mainCtrl', function($scope, $timeout) {
+  $scope.saySomethingPicard = "Beam me up, Scotty!";
+
+  $scope.picardsWords = "Yes you can";
+  $timeout(function() {
+    $scope.picardsWords = "This is bad, warp speed escape!";
+  }, 2000);
+
+  $scope.crew = [
+    {name: "Picard", rank: "Captain"},
+    {name: "Data", rank: "Data guy"},
+    {name: "Scotty", rank: "Dog"},
+    {name: "Worf", rank: "Shipping Lead"},
+    {name: "Spock", rank: "Speck"}
+  ];
+
+  $scope.isAwesome = true;
+
+  $scope.btnClicked = function(){
+                           $scope.isAwesome=!$scope.isAwesome;
+ };
+});
